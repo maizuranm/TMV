0d4a1d222b4f4142f09fab720f73c7ba85911b16
Maizura <maizura@nm.gov.my>
2025-10-02 17:29:35 +0200

    Update download-snapshot-llama4.py
    Update the comments

--- download-snapshot-llama4.py
+++ download-snapshot-llama4.py
@@ -2,10 +2,9 @@ from huggingface_hub import snapshot_download
 
 snapshot_download(
     repo_id="meta-llama/Llama-4-Maverick-17B-128E-Instruct",
-    local_dir="/home/zura/models/llama4_maverick_bf16"
+    local_dir="/home/zura/models/llama4_maverick_bf16" #input the path directory where the model should be downloaded
 )
 
 snapshot_download(
     repo_id="meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8",
-    local_dir="/home/zura/models/llama4_maverick_fp8"
-)
+    local_dir="/home/zura/models/llama4_maverick_fp8" #input the path directory where the model should be downloaded)
